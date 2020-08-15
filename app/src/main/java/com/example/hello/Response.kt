a015bcfd0b89f25ba57dcb427f020fa7210ed28f
monicahnyokabi <wambuinyokabimn@gmail.com>
2020-08-15 08:37:51 +0300

    first commit

---
+++ app/src/main/java/com/example/hello/Response.kt
@@ -0,0 +1,17 @@
+package com.example.hello
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.gson.annotations.SerializedName
+
+class Response : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_response)
+        data class RegistrationResponse(
+            @SerializedName("message") var message: String,
+            @SerializedName("student") var student: Student
+        )
+
+    }
+}
---
+++ app/src/main/java/com/example/hello/Student.kt
@@ -0,0 +1,21 @@
+package com.example.hello
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.gson.annotations.SerializedName
+
+class Student : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_student4)
+        data class Student(
+            @SerializedName("student_id") var studentId: String,
+            @SerializedName("first_name") var firstName: String,
+            @SerializedName("last_name") var lastName: String,
+            @SerializedName("email") var email: String,
+            @SerializedName("phone_number") var phoneNumber: String,
+            @SerializedName("image_url") var imageUrl: String
+        )
+
+    }
+}
